b8b3c1a54dc17d0737e9705703586b547a9b28de
lqy <lqy@100bsh.com>
2020-01-03 13:06:48 +0800

    修改备案号

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -10,7 +10,7 @@
             <p><b>站点声明：</b></p>
             <p>1、所有文章未经授权禁止转载、摘编、复制或建立镜像，如有违反，追究法律责任。举报邮箱：<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=543492227@qq.com" target="_blank">543492227@qq.com</a></p>
             <p>2、如有未经允许授权转载的文章可联系作者下架。作者邮箱：<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=543492227@qq.com" target="_blank">543492227@qq.com</a></p>
-            <p>Copyright © <a href="http://www.lqy-comic.com" target="_blank">www.lqy-comic.com</a> All Rights Reserved. 备案号：<a href="http://www.beian.gov.cn/portal/registerSystemInfo" target="_blank">皖ICP备18005512号-1</a></p>
+            <p>Copyright © <a href="http://www.lqy-comic.com" target="_blank">www.lqy-comic.com</a> All Rights Reserved. 备案号：<a href="http://beian.miit.gov.cn/" target="_blank">皖ICP备18005512</a></p>
         </div>
     </div>
     <a href="#">
