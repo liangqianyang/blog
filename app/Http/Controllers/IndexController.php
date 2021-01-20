838d75380f6652b76a344294d3aca44cb14355f4
liangqianyang <liangqianyang@114study.com>
2021-01-20 13:50:30 +0800

    修改接口

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -38,7 +38,7 @@ class IndexController extends Controller
             $category_id = $categories->get(0)->id;
             $columns = ['id', 'cover', 'title', 'summary'];
             $articles = $articleService->getArticleByCategory($category_id, $columns, 5);
-            if (!empty($articles)) {
+            if (count($articles)>0) {
                 $tab['list'] = $articles;
                 $tab['pic'] = $articles->random(2);
             }
